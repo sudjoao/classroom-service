c5abcf33f4dd58c0b7376b71ec2911a56374b31c
sudjoao <isudjoao@gmail.com>
2024-11-10 09:44:57 -0300

    build: start project

---
+++ src/main/java/com/sudjoao/classroom_service/ClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sudjoao.classroom_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudjoao/classroom_service/ClassroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudjoao.classroom_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
